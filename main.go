75085e9bcadde1469283e22bb393be38546d9df2
Jonas Falck <jonas.falck@gmail.com>
2016-07-06 14:49:34 +0200

    Return wsdl if requested

--- main.go
+++ main.go
@@ -5,19 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
 
 var (
-	port     string
-	response string
-	sleep    int
+	port         string
+	response     string
+	wsdlResponse string
+	sleep        int
 )
 
 func init() {
-	flag.StringVar(&port, "p", "8080", "Port to listen on")
-	flag.StringVar(&response, "r", "", "response so answer with")
-	flag.IntVar(&sleep, "d", 0, "Delay before responding")
+	flag.StringVar(&port, "port", "8080", "Port to listen on")
+	flag.StringVar(&response, "response", "", "response so answer with")
+	flag.StringVar(&wsdlResponse, "wsdl", "", "response so answer on ?WSDL")
+	flag.IntVar(&sleep, "delay", 0, "Delay before responding with response")
 }
 
 func main() {
@@ -28,7 +31,23 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("| %s | %s | %s", r.RemoteAddr, r.Method, r.RequestURI)
+	if getWsdl(r.URL) {
+		fmt.Fprint(w, wsdlResponse)
+		return
+	}
+
 	time.Sleep(time.Duration(sleep) * time.Millisecond)
 	fmt.Fprint(w, response)
-	log.Printf("| %s | %s | %s", r.RemoteAddr, r.Method, r.RequestURI)
+}
+
+func getWsdl(u *url.URL) bool {
+
+	if _, ok := u.Query()["WSDL"]; ok {
+		return true
+	}
+	if _, ok := u.Query()["wsdl"]; ok {
+		return true
+	}
+	return false
 }
